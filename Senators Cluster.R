b7b5b5cba9ed0db2ee75b37fd6dfcbe90147bf54
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-29 14:05:46 +1000

    senators cluster-finalise

--- Senators Cluster.R
+++ Senators Cluster.R
@@ -52,3 +52,22 @@ dev.off()
 png('clu_2.png')
 print(base.110+geom_text(aes(color=party, alpha=0.75, label=cong.110$name, size=2)))
 dev.off()
+
+# all plots
+all.mds <- do.call(rbind, rollcall.mds)
+all.plot <- ggplot(all.mds, aes(x=x, y=y))+
+    geom_point(aes(shape=party, alpha=0.75, size=2))+
+    
+    scale_alpha(guide="none")+theme_bw()+
+    theme(axis.ticks=element_blank(), axis.text.x=element_blank(),
+         axis.text.y=element_blank(),
+         title=element_text("Roll Call Vote MDS Clustering for U.S. Senate
+(101st - 111th Congress)"),
+         panel.grid.major=element_blank())+
+    xlab("")+ylab("")+
+    scale_shape(name="Party", breaks=c("100","200","328"),
+                labels=c("Dem.", "Rep.", "Ind."),
+                solid=FALSE)+facet_wrap(~ congress)
+png('clu_3.png',height = 800,width = 1050)
+all.plot
+dev.off()
