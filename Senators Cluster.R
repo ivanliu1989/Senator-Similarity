43727c5985e060cc093029630c09104a5849c6c1
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-29 14:00:13 +1000

    senators cluster-visualiztion

--- Senators Cluster.R
+++ Senators Cluster.R
@@ -9,6 +9,7 @@ rollcall.data <- lapply(data.files,
 dim(rollcall.data[[1]])
 head(rollcall.data[[1]])
 
+# proprocessing
 rollcall.simplified <- function(df){
     no.pres <- subset(df,state<99)
     for(i in 10:ncol(no.pres)){
@@ -18,4 +19,36 @@ rollcall.simplified <- function(df){
     }
     return(as.matrix(no.pres[,10:ncol(no.pres)]))
 }
+
+# distance and mds
 rollcall.simple<-lapply(rollcall.data,rollcall.simplified)
+
+rollcall.dist <- lapply(rollcall.simple,function(m)dist(m %*% t(m)))
+rollcall.mds <- lapply(rollcall.dist,function(d)as.data.frame((cmdscale(d,k=2))*-1))
+
+# visualization
+congresses <- 101:111
+for (i in 1:length(rollcall.mds)){
+    names(rollcall.mds[[i]])<-c('x','y')
+    congress <- subset(rollcall.data[[i]],state<99)
+    congress.names <- sapply(as.character(congress$name),
+                             function(n) strsplit(n,'[, ]')[[1]][1])
+    rollcall.mds[[i]]<-transform(rollcall.mds[[i]],name=congress.names,
+                                 party=as.factor(congress$party),congress=congresses[i])
+}
+head(rollcall.mds[[1]])
+
+cong.110 <- rollcall.mds[[9]]
+base.110 <- ggplot(cong.110, aes(x=x,y=y))+
+    scale_alpha(guide="none")+theme_bw()+
+    theme(axis.ticks=element_blank(),axis.text.x=element_blank(),
+          axis.text.y=element_blank(),title=element_text('Roll Call Vote MDS Clustering for 110th U.S. Senate'),
+          panel.grid.major=element_blank())+xlab('')+ylab('')+
+    scale_color_manual(name='Party',values=c('100'='black','200'='dimgray','328'='grey'),
+                       breaks=c('100','200','328'),labels=c('Dem.','Rep.','Ind,'))
+png('clu_1.png')
+print(base.110+geom_point(aes(shape=party,alpha=.75,size=2)))
+dev.off()
+png('clu_2.png')
+print(base.110+geom_text(aes(color=party, alpha=0.75, label=cong.110$name, size=2)))
+dev.off()
