2d947f30597f1084044b3311ae208032fd99d890
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-29 13:36:37 +1000

    senators cluster-preprocessing

--- Senators Cluster.R
+++ Senators Cluster.R
@@ -8,3 +8,14 @@ rollcall.data <- lapply(data.files,
 
 dim(rollcall.data[[1]])
 head(rollcall.data[[1]])
+
+rollcall.simplified <- function(df){
+    no.pres <- subset(df,state<99)
+    for(i in 10:ncol(no.pres)){
+        no.pres[,i]<-ifelse(no.pres[,i]>6,0,no.pres[,i])
+        no.pres[,i]<-ifelse(no.pres[,i]>0 & no.pres[,i]<4,1,no.pres[,i])
+        no.pres[,i]<-ifelse(no.pres[,i]>1,-1,no.pres[,i])
+    }
+    return(as.matrix(no.pres[,10:ncol(no.pres)]))
+}
+rollcall.simple<-lapply(rollcall.data,rollcall.simplified)
