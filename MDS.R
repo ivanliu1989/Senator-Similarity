1c8342ede40556092e2132a737644f862a3cc1aa
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-29 13:05:43 +1000

    initializing

--- MDS.R
+++ MDS.R
@@ -1,1 +1,7 @@
 setwd("C:\\Documents and Settings\\Macro\\Desktop\\Ivandata\\Senator-Similarity")
+set.seed(851982)
+ex.matrix <- matrix(sample(c(-1,0,1),24,replace=T),nrow=4,ncol=6)
+row.names(ex.matrix)<- c('A','B','C','D')
+colnames(ex.matrix)<-c('P1','P2','P3','P4','P5','P6')
+
+# matrix transposition and multiplication
